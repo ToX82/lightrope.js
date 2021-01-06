8a79ba70c5641a85c78194f2cd53ba547c22d867
Emanuele "ToX" Toscano <toss82@gmail.com>
2021-01-06 20:38:55 +0100

    Automatic css loading

--- lightrope.js
+++ lightrope.js
@@ -1,21 +1,42 @@
-// First, we add the lightrope CSS
-var stylesheet = document.createElement('link');
+// First, we add the lightrope CSS to the DOM
+let scriptPath = getScriptPath();
+let stylesheet = document.createElement('link');
 stylesheet.setAttribute('rel', 'stylesheet');
-stylesheet.setAttribute('href', 'lightrope.css');
+stylesheet.setAttribute('href', scriptPath + 'lightrope.css');
 document.head.appendChild(stylesheet);
 
-// How meny lights are required to fill the window's width?
-var width = window.outerWidth;
-var lights = width / 28;
+// Then we calculate how meny lights are required to fill the window's width
+let width = window.outerWidth;
+let lights = (width / 28) + 1;
 
 // Create the "rope" container
-var body = document.querySelector('body');
-var rope = document.createElement('ul');
+let body = document.querySelector('body');
+let rope = document.createElement('ul');
 rope.className = 'lightrope';
 body.prepend(rope);
 
 // And finally, add the light bulbs!
-for (var i = 0; i <= lights; i++) {
-    var bulb = document.createElement('li');
+for (let i = 0; i < lights; i++) {
+    let bulb = document.createElement('li');
     rope.append(bulb);
+}
+
+/**
+* Get this javascript's path
+* @return {String} this javascript's path
+*/
+function getScriptPath() {
+    let scripts = document.getElementsByTagName('script');
+
+    for (i = 0; i < scripts.length; i += 1) {
+        if (scripts[i].hasAttribute('src')) {
+            path = scripts[i].src;
+            if (path.indexOf('lightrope') > -1) {
+                path = path.replace(/[^\/]*$/, '');
+                return path;
+            }
+        }
+    }
+
+    return '';
 }
